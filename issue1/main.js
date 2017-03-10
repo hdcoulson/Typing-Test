63f12f5b8a91eb4864b0b96eb3c0aeee46e3b066
Harry Coulson <coulson.mail@me.com>
2017-03-10 14:46:53 -0800

    Updated code to use javaScript file to produce the sentence.

--- issue1/main.js
+++ issue1/main.js
@@ -1,1 +1,6 @@
-var text = 'The brown fox is quick.'
+window.onload = function() {
+  var sentence = document.createElement("p");
+  var sentence_text = document.createTextNode("The brown fox is quick");
+  sentence.appendChild(sentence_text);
+  document.body.appendChild(sentence);
+  }
