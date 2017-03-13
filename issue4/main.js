ad3c8e7e2d7b0711a38edf95895a5ab68580a689
Harry Coulson <coulson.mail@me.com>
2017-03-13 15:28:25 -0700

    Completion of typing-test assignment.

--- issue4/main.js
+++ issue4/main.js
@@ -1,5 +1,7 @@
 //assign the sentece string to a variable//
 var testSentence = "The brown fox is quick."
+var numberWrong = 0
+var correctlyTyped = 1
 //place the variable in an array on the page//
 for (var i =0; i < testSentence.length; i++) {
 var $letter = document.createElement('span')
@@ -12,19 +14,25 @@ $firstletter.classList.add('current')
 //Take user input with eventlisteners/
 document.addEventListener('keypress', function(event) {
   console.log(event.key)
-  var $currentKey = document.querySelector('.current')
-  var $currentCharacter = $currentKey.textContent
-  if (event.key === $currentCharacter) {
-    $currentKey.classList.remove('current','wrong')
-    $currentKey.nextSibling.classList.add('current')
-    } else {
-      $currentKey.classList.add('wrong')
+  if (correctlyTyped === testSentence.length){
+    showScore()
   }
-})
-document.addEventListener('counter', function(???) {
-  var numberWrong = 0
-  for (var i = 0; i < ???; i++) {
-    if ()
-    }
-    return 'Number Incorrect' + numberWrong
-  })
+  else {
+      var $currentKey = document.querySelector('.current')
+      var $currentCharacter = $currentKey.textContent
+        if (event.key === $currentCharacter) {
+          correctlyTyped++
+          $currentKey.classList.remove('current','wrong')
+          $currentKey.nextSibling.classList.add('current')
+        } else {
+          $currentKey.classList.add('wrong')
+          numberWrong = numberWrong+1
+        }
+      }
+    })
+ function showScore()
+   {
+     var $score = document.createElement('div')
+     $score.textContent = 'Number of errors ' + ' ' + numberWrong
+     document.body.appendChild($score)
+   }
